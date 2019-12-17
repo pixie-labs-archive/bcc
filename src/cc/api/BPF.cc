3051e39249997ba00ef67ac832a0faf757634926
Yaxiong Zhao <yzhao@pixielabs.ai>
2019-12-16 17:05:45 -0800

    Fix bug where the range was treated as right-open
    In reality, it's a closed range.

--- src/cc/api/BPF.cc
+++ src/cc/api/BPF.cc
@@ -677,7 +677,7 @@ namespace {
 std::string random_alnum_string(int len) {
   static constexpr char kDict[] = "0123456789abcdefghijklmnopqrstuvwxyz";
   static std::random_device rd;
-  std::uniform_int_distribution<size_t> dist(0, sizeof(kDict));
+  std::uniform_int_distribution<size_t> dist(0, sizeof(kDict)-1);
   std::string res;
   res.reserve(len);
   for (int i = 0; i < len; ++i) {
